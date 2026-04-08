80c327758d1ee8ecf7d73395f758b380779562fb
yashwanth7654321-creator <yashwanth7654321@gmail.com>
2026-04-08 14:18:56 +0530

    loading pdf

--- source_code/loader.py
+++ source_code/loader.py
@@ -1,4 +1,5 @@
 import os
+from PyPDF2 import PdfReader
 from config import TEXT_DATA_PATH 
 
 def load_documents():
@@ -8,4 +9,18 @@ def load_documents():
              with open(os.path.join(TEXT_DATA_PATH, doc), "r", encoding="utf-8") as f:
                 text = f.read()
                 docs.append({"text": text, "source": doc, "page": None})
+                
+        elif doc.endswith(".pdf"):
+            reader = PdfReader(os.path.join(TEXT_DATA_PATH, doc))
+
+            for page_num, page in enumerate(reader.pages):
+                text = page.extract_text()
+
+                if text:  # avoid empty pages
+                    docs.append({
+                        "text": text,
+                        "source": doc,
+                        "page": page_num + 1
+                    })
+
      return docs
