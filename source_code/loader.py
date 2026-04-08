932562d17fdb6a10c3c436c0fe682b96dfcbf161
yashwanth7654321-creator <yashwanth7654321@gmail.com>
2026-04-08 14:20:05 +0530

    Indentaion issue fixed

--- source_code/loader.py
+++ source_code/loader.py
@@ -3,19 +3,17 @@ from PyPDF2 import PdfReader
 from config import TEXT_DATA_PATH 
 
 def load_documents():
-     docs = []
-     for doc in os.listdir(TEXT_DATA_PATH):
-         if doc.endswith(".txt"):
-             with open(os.path.join(TEXT_DATA_PATH, doc), "r", encoding="utf-8") as f:
+    docs = []
+    for doc in os.listdir(TEXT_DATA_PATH):
+        if doc.endswith(".txt"):
+            with open(os.path.join(TEXT_DATA_PATH, doc), "r", encoding="utf-8") as f:
                 text = f.read()
                 docs.append({"text": text, "source": doc, "page": None})
-                
+
         elif doc.endswith(".pdf"):
             reader = PdfReader(os.path.join(TEXT_DATA_PATH, doc))
-
             for page_num, page in enumerate(reader.pages):
                 text = page.extract_text()
-
                 if text:  # avoid empty pages
                     docs.append({
                         "text": text,
@@ -23,4 +21,4 @@ def load_documents():
                         "page": page_num + 1
                     })
 
-     return docs
+    return docs
